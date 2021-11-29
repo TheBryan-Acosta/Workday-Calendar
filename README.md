5c11466a21205a868ee0c6ab55ccc0c45c99c653
Bryan A <bryanacostadelacruz@gmail.com>
2021-11-29 13:38:09 -0600

    save userinput to localstorage + readme + comments

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler
+
+GitHub Link : https://github.com/TheBryan-Acosta/Workday-Calendar
+GitPages Link : https://thebryan-acosta.github.io/Workday-Calendar/
+
+## **Description**
+
+Challenge 05 This Weeks challenge was to create a Work Scheduler
+
+### **What was developed**
+
+A fully Work Scheduler, styled with bootstrap.
+for more indepth explaination view the script.js and index.html comments
+
+### **Development** -
+
+Using Jquery, Momentjs, and bootstrap, I first fixed some responsivness issues from the starter code, laid out the html and css for mobile first. then proceeded write the script to update hourly tasks written by the user, which is stored in local storage. view script.js for a technical explaination.
+
+## **Examples**
+
+## **Installation**
+
+Developed with HTML, CSS, js through Visual Studio Code.
+
+## **Usage**
+
+Plan out your work day with a hot new work scheduler!
+
+## **Credits**
+
+University of Texas Coding Bootcamp
+
+## **License**
+
+MIT License
+
+Copyright (c) [2021] [Bryan Acosta De La Cruz]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,58 @@
+/*----------------------------------------------------------------------*/
+// create variables for the current hour and day
+// set the text in the element that has the id of currentDay to the current day
+/*----------------------------------------------------------------------*/
 var currentDay = moment().format('dddd, MMMM Do');
 var currentHour = parseInt(moment().format('HH'));
+var userDescriptions = Array(9).fill('');
 
 $("#currentDay").text(currentDay);
 
-//set colors to correspond what is due, past due, and upcoming
+/*----------------------------------------------------------------------*/
+// parse stringified localStorage to usable data
+// if returns falsy just fill it with '' 
+// then iterate through all descriptions and fill text with their
+// matching index from local storage
+/*----------------------------------------------------------------------*/
+loadDescriptions();
+function loadDescriptions(){
+  userDescriptions = JSON.parse(localStorage.getItem("userDescriptions"));
+  
+  if(!userDescriptions){
+      userDescriptions = Array(9).fill('');
+  }
+  
+  $(".description").each(function(index){
+    $(this).find('p').text(userDescriptions[index]);
+  });
+}
+/*----------------------------------------------------------------------*/
+// set colors to correspond what is due, past due, and upcoming.
+// while im iterating through all the time blocks im going to give them an Id of their index
+// am and pm dont exist mathimatically use so use military time
+// to compare real time vs the set intervals I started the index at 9 iterating up
+/*----------------------------------------------------------------------*/
 function updateColors(){
     $(".description").each(function(index) {
         var hour = index + 9;
         if(hour < currentHour){
             $(this).css("background-color", "#d3d3d3");
         }
-
         else if (hour == currentHour){
             $(this).css("background-color", "#ff6961");
         }
-
         else {
             $(this).css("background-color", "#77dd77");
         }
+
+        $(this).attr('id', index);
     });
 }
-// update colors once and then every minute + day
+/*----------------------------------------------------------------------*/
+// set a Interval function to run every minute
+// update colors once and then every minute
+// update the current day each minute
+/*----------------------------------------------------------------------*/
 updateColors();
 setInterval(function () {
     currentDay = moment().format('dddd, MMMM Do');
@@ -28,27 +60,44 @@ setInterval(function () {
     currentHour = parseInt(moment().format('HH'));
     updateColors();
 }, (1000 * 60));
-
-//enable editing tasks
+/*----------------------------------------------------------------------*/
+// editing tasks.
+// select the existing text, create a text input form with said text, replace.
+// traverse to the parent of the parent to the p tag to get the current timeBlock
+// find the lock icon in the timeblock, then switch the class to unlocked
+/*----------------------------------------------------------------------*/
 $(".description").on("click", "p", function() {
-  // get current text of p element
-  var text = $(this).text().trim();
+    var text = $(this).text().trim();
+    var timeblock = $(this).parent().parent();
 
-  // replace p element with a new textarea
-  var textInput = $("<textarea>").addClass("form-control").val(text);
-  $(this).replaceWith(textInput);
+    var icon = $(timeblock).find('.bi', 'bi-lock');
+    $(icon).attr('class', 'bi bi-unlock');
+
+    var textInput = $("<textarea>").addClass("form-control").val(text);
+    $(this).replaceWith(textInput);
 
 });
-
+/*----------------------------------------------------------------------*/
+// grab the existing text, create a p tag with said text, replace.
+// on save button click, traverse the icon through the parent and change class to locked
+// get the id and text of description
+// override the description at the index of the localStorage array. set and stringify
+/*----------------------------------------------------------------------*/
 $(".time-block").on("click", ".saveBtn", function(){
     var timeblock = $(this).parent();
-    var description = $(timeblock).find(".form-control").val();
+    var descriptionText = $(timeblock).find(".form-control").val();
+    var taskP = $("<p>").text(descriptionText);
+    var Descpid = $(timeblock).find(".description").attr('id');
+    var icon = $(timeblock).find('.bi', 'bi-unlock');
 
-    
-    var taskP = $("<p>")
-    .text(description);
-
+    $(icon).attr('class', 'bi bi-lock');
   // replace textarea with new content
-  $(timeblock).find(".form-control").replaceWith(taskP);
-    
+    $(timeblock).find(".form-control").replaceWith(taskP);
+
+    if(!Descpid || !descriptionText){
+        return false;
+    }
+    userDescriptions[Descpid] = descriptionText;
+    localStorage.setItem("userDescriptions", JSON.stringify(userDescriptions));
 });
+/*----------------------------------------------------------------------*/
